09100770c0d00d5775df4e9bd755081319b170e4
Chauncey Philpot <chauncey.philpot@IALPOV.US>
2019-11-19 15:07:19 -0500

    fixed typo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const mongoose = require('mongoose');
+
 module.exports = {
     client: require('./model/client.js'),
-    token: require('./model/client.js'),
+    token: require('./model/token.js'),
     user: require('./model/user')
 }
