2e41850b4db882f87fe4483a15b4db6edc31ae34
Chauncey Philpot <chauncey.philpot@IALPOV.US>
2019-11-21 10:23:13 -0500

    allowing alternate models for a collection

--- model/user.js
+++ model/user.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
 	modelName = 'user',
 	schemaDefinition = require('../schema/' + modelName),
-	schemaInstance = mongoose.Schema(schemaDefinition),
+	schemaInstance = mongoose.Schema(schemaDefinition, { collection: 'users' }),
 	modelInstance = mongoose.model(modelName, schemaInstance);
 
 module.exports = modelInstance;
