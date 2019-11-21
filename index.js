86a26335dc6225e9cb6e859cec5ce62291ef370d
Chauncey Philpot <chauncey.philpot@IALPOV.US>
2019-11-21 09:03:36 -0500

    added export

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose');
 module.exports = {
     client: require('./model/client.js'),
     token: require('./model/token.js'),
-    user: require('./model/user')
+    user: require('./model/user'),
+    authUser: require('./model/user')
 }
