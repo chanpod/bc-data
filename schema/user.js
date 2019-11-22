6143ddb10ad0b171f0edc26e9874c8493375805e
Chauncey Philpot <chauncey.philpot@IALPOV.US>
2019-11-22 14:00:04 -0500

    finally know how to exclude a schema field

--- schema/user.js
+++ schema/user.js
@@ -1,7 +1,9 @@
-module.exports = {    
+module.exports = {
     username: String,
     email: String,
     firstName: String,
+    password: { type: String, select: false },
+    salt: { type: String, select: false },
     lastName: String,
     createdOn: Date,
     updatedOn: Date,
